b8959a4f7942e172c8a9e966259803f8756c6313
Karl Horning <karl_horning@Karls-iMac.local>
2018-09-05 23:03:51 +0100

    Validation and some validation styling added

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, session
+from flask import flash, Flask, redirect, render_template, session, url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -11,26 +11,29 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 # Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
+if not os.getenv('DATABASE_URL'):
+    raise RuntimeError('DATABASE_URL is not set')
 
 # Configure session to use filesystem
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine(os.getenv('DATABASE_URL'))
 db = scoped_session(sessionmaker(bind=engine))
 
 
-@app.route("/")
-@app.route("/index")
+@app.route('/')
+@app.route('/index')
 def index():
     user = {'username': 'Karl'}
     return render_template('index.html', user=user)
 
-@app.route("/login")
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect(url_for('index'))        
     return render_template('login.html', form=form)
