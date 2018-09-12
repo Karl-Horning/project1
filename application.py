3020ba54f3f6940114bbc7c28c6d2b8b2aa2aaca
Karl Horning <karl_horning@Karls-iMac.local>
2018-09-12 22:57:35 +0100

    Replaced fetchall with fetchone when getting data for a single book

--- application.py
+++ application.py
@@ -19,7 +19,7 @@ def index():
 @app.route('/books/<isbn>')
 def books(isbn):
     """List the details of a book"""
-    book =  db.engine.execute(f"SELECT * FROM books WHERE isbn = '{isbn}'").fetchall()
+    book =  db.engine.execute(f"SELECT * FROM books WHERE isbn = '{isbn}'").fetchone()
     if book is None:
         return render_template('error.html', message=f'No book with the ID {isbn} exists.')
 
@@ -31,6 +31,12 @@ def books(isbn):
         number_of_ratings = locale.format('%d', res.json()['books'][0]['work_ratings_count'], grouping=True)
 
     return render_template('book.html', book=book, Goodreads=Goodreads)
+
+@app.route('/api/<isbn>')
+def api(isbn):
+    """List the book's API"""
+    book =  db.engine.execute(f"SELECT * FROM books WHERE isbn = '{isbn}'").fetchone()
+    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": Config.KEY, "isbns": isbn})
 
 @app.route('/test')
 def test():
