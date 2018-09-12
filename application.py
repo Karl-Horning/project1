21f6e18e6d409f05e7e1ce16ca13fecb60e0981c
Karl Horning <karl_horning@Karls-iMac.local>
2018-09-12 22:45:54 +0100

    Execute method added for database queries, test page added, books are now queried via their ISBN

--- application.py
+++ application.py
@@ -13,31 +13,45 @@ locale.setlocale(locale.LC_ALL, 'en_GB')
 
 @app.route('/')
 def index():
-    books = Book.query.limit(25).all()
+    books = db.engine.execute('SELECT * FROM books LIMIT 50')
     return render_template('index.html', books=books)
 
-# @app.route('/books/<book_isbn>')
-# def books(book_isbn):
-#     """List the details of a book"""
-#     book = Book.query.with_entities(Book.isbn)
-#     # book = Book.query.get(book_id)
-#     if book is None:
-#         return render_template('error.html', message=f'No book with the ISBN {book_isbn} exists.')
-
-#     return render_template('book.html', book=book)
-
-@app.route('/books/<int:book_id>')
-def books(book_id):
+@app.route('/books/<isbn>')
+def books(isbn):
     """List the details of a book"""
-    book = Book.query.get(book_id)
+    book =  db.engine.execute(f"SELECT * FROM books WHERE isbn = '{isbn}'").fetchall()
     if book is None:
-        return render_template('error.html', message=f'No book with the ID {book_id} exists.')
+        return render_template('error.html', message=f'No book with the ID {isbn} exists.')
 
     # Make request to Goodreads API
-    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": Config.KEY, "isbns": book.isbn})
+    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": Config.KEY, "isbns": isbn})
     # Create an object with the needed JSON data
     class Goodreads(object):
         average_rating = res.json()['books'][0]['average_rating']
         number_of_ratings = locale.format('%d', res.json()['books'][0]['work_ratings_count'], grouping=True)
 
     return render_template('book.html', book=book, Goodreads=Goodreads)
+
+# @app.route('/books/<int:book_id>')
+# def books(book_id):
+#     """List the details of a book"""
+#     book = Book.query.get(book_id)
+#     if book is None:
+#         return render_template('error.html', message=f'No book with the ID {book_id} exists.')
+
+#     # Make request to Goodreads API
+#     res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": Config.KEY, "isbns": book.isbn})
+#     # Create an object with the needed JSON data
+#     class Goodreads(object):
+#         average_rating = res.json()['books'][0]['average_rating']
+#         number_of_ratings = locale.format('%d', res.json()['books'][0]['work_ratings_count'], grouping=True)
+
+#     return render_template('book.html', book=book, Goodreads=Goodreads)
+
+@app.route('/test')
+def test():
+    q = 'dav'
+    # books = db.engine.execute("SELECT * FROM books WHERE LOWER(author) = LOWER('Henry James')")
+    # books = db.engine.execute(f"SELECT * FROM books WHERE LOWER(author) LIKE LOWER('%%{q}%%')")
+    books = db.engine.execute(f"SELECT * FROM books WHERE isbn = '0316010766'")
+    return render_template('test.html', books=books)
